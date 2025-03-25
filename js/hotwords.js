7ca4e8b1326eac5e00b83edd3bd20dc73ec5e2f5
nsrivast <nsrivast@gmail.com>
2025-03-24 22:04:22 -0400

    New content and styling

--- js/hotwords.js
+++ js/hotwords.js
@@ -1,16 +1,27 @@
 $(document).ready(function () {
   $(".hot").hover(
     function () {
-      $(".content").mark(this.getAttribute("data-keyword"));
+      const keyword = this.getAttribute("data-keyword");
+      const highlightClass = `highlight-${keyword}`;
+      
+      // Find and highlight all elements with the corresponding highlight class
+      $(`.${highlightClass}`).each(function() {
+        // Mark the entire text content as one unit
+        // This preserves spaces between words in the highlight
+        $(".content").mark($(this).text(), {
+          "accuracy": "exactly",
+          "separateWordSearch": false
+        });
+      });
     },
     function () {
       $(".content").unmark();
     }
   );
 
-  $(".arrow").on("click", function (event) {
+  $(".section-arrow").on("click", function (event) {
     $(this).find("img").toggleClass("rotate");
     $(this).find("img").toggleClass("rotate-reset");
-    $(".detail").toggleClass("show");
+    $(this).next(".detail").toggleClass("show");
   });
 });
